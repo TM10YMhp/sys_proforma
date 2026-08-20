bacae7f123d434ca3c6345ae1a2bed65a238117a
tm10ymhp <tm10ymhp.editor919@slmails.com>
2026-08-20 11:01:28 -0500

    feat: agregar migracion para tabla de productos

---
+++ database/migrations/2026_08_20_152356_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('products', function (Blueprint $table) {
+      $table->id();
+
+      $table->string('nombre')->unique();
+      $table->text('descripcion')->unique();
+      $table->decimal('precio', 10, 2);
+      $table->string('unidad_medida');
+      $table->unsignedInteger('stock')->default(0);
+      $table->boolean('activo');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('products');
+  }
+};
