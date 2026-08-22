bb3ad8745f6cbb340dd2b6e022f4abe72cc6c35e
tm10ymhp <tm10ymhp.editor919@slmails.com>
2026-08-22 10:38:45 -0500

    refactor: finalizar vista del listado de productos

--- resources/js/pages/products/index.svelte
+++ resources/js/pages/products/index.svelte
@@ -4,7 +4,7 @@
   export const layout = {
     breadcrumbs: [
       {
-        title: 'Products',
+        title: 'Productos',
         href: index.url(),
       },
     ],
@@ -13,133 +13,122 @@
 
 <script lang="ts">
   import AppHead from '@/components/AppHead.svelte';
+  import { Link, useForm } from '@inertiajs/svelte';
+  import Button from '@/components/ui/button/Button.svelte';
+  import ProductController from '@/actions/App/Http/Controllers/ProductController';
   import type { LaravelPaginator } from '@/types/paginate';
-
-  interface Product {
-    id: number;
-    nombre: string;
-    descripcion: string;
-    precio: number;
-    unidad_medida: string;
-    stock: number;
-    activo: boolean;
-    // TODO: check this
-    created_at: string;
-    updated_at: string;
-  }
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from '@/components/_ui/table';
+  import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from '@/components/_ui/pagination';
+  import type { Product } from '@/types/product';
 
   type Props = {
     products: LaravelPaginator<Product>;
   };
   let { products }: Props = $props();
+
+  const form = useForm();
+
+  const handleDelete = (id: number) => {
+    if (confirm('Estas seguro que deseas eliminar este producto?')) {
+      form.delete(ProductController.destroy.url(id));
+    }
+  };
 </script>
 
 <AppHead title="Productos" />
 
 <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-  <div
-    class="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default"
-  >
-    <table class="w-full text-sm text-left rtl:text-right text-body">
-      <thead
-        class="text-sm text-body bg-neutral-secondary-medium border-b border-default-medium"
-      >
-        <tr>
-          <th scope="col" class="px-6 py-3 font-medium">Nombre</th>
-          <th scope="col" class="px-6 py-3 font-medium">Descripcion</th>
-          <th scope="col" class="px-6 py-3 font-medium">Precio</th>
-          <th scope="col" class="px-6 py-3 font-medium">Unidad de Medida</th>
-          <th scope="col" class="px-6 py-3 font-medium">Stock</th>
-          <th scope="col" class="px-6 py-3 font-medium">Activo</th>
-          <th scope="col" class="px-6 py-3 font-medium">Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each products.data as item (item.id)}
-          <tr
-            class="bg-neutral-primary-soft border-b border-default hover:bg-neutral-900"
-          >
-            <th
-              scope="row"
-              class="px-6 py-4 font-medium text-heading whitespace-nowrap"
+  <!-- TODO: el cursor no se establece -->
+  <Link href={ProductController.create.url()} class="w-fit">
+    <Button>Nuevo Producto</Button>
+  </Link>
+
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Nombre</TableHead>
+        <TableHead>Descripcion</TableHead>
+        <TableHead>Precio</TableHead>
+        <TableHead>Unidad de Medida</TableHead>
+        <TableHead>Stock</TableHead>
+        <TableHead>Activo</TableHead>
+        <TableHead>Acciones</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {#each products.data as item (item.id)}
+        <TableRow>
+          <TableCell>{item.nombre}</TableCell>
+          <TableCell>{item.descripcion}</TableCell>
+          <TableCell>{item.precio}</TableCell>
+          <TableCell>{item.unidad_medida}</TableCell>
+          <TableCell>{item.stock}</TableCell>
+          <TableCell>{item.activo}</TableCell>
+          <TableCell>
+            <Link href={ProductController.edit(item.id)}>
+              <Button class="bg-slate-500 hover:bg-slate-700">Editar</Button>
+            </Link>
+            <Button
+              disabled={form.processing}
+              class="bg-red-500 hover:bg-red-700"
+              onclick={() => handleDelete(item.id)}
             >
-              {item.nombre}
-            </th>
-            <td class="px-6 py-4">{item.descripcion}</td>
-            <td class="px-6 py-4">{item.precio}</td>
-            <td class="px-6 py-4">{item.unidad_medida}</td>
-            <td class="px-6 py-4">{item.stock}</td>
-            <td class="px-6 py-4">{item.activo}</td>
-            <td class="px-6 py-4 text-right"> TODO </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-    <nav
-      class="flex flex-col items-start justify-between p-4 space-y-3 md:flex-row md:items-center md:space-y-0"
+              Borrar
+            </Button>
+          </TableCell>
+        </TableRow>
+      {/each}
+    </TableBody>
+  </Table>
+
+  <div class="flex flex-row justify-between p-4">
+    <span class="text-gray-400"
+      >Mostrando <span class="font-semibold text-white"
+        >{products.from}-{products.to}</span
+      >
+      de
+      <span class="text-white">{products.total}</span></span
     >
-      <span class="text-gray-400"
-        >Mostrando <span class="font-semibold text-white"
-          >{products.from}-{products.to}</span
-        >
-        de
-        <span class="text-white">{products.total}</span></span
-      >
-      <ul class="inline-flex items-stretch -space-x-px">
-        <li>
-          <a
-            href={products.prev_page_url}
+    <Pagination class="mx-0 w-auto">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
             class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span class="sr-only">Previous</span>
-            <svg
-              class="w-5 h-5"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-        </li>
+            href={products.prev_page_url}
+          />
+        </PaginationItem>
         {#each products.links as item (item.label)}
           {#if !item.label.includes('Previous') && !item.label.includes('Next')}
-            <li>
-              <a
+            <PaginationItem>
+              <PaginationLink
                 href={item.url}
                 class={`flex items-center justify-center px-3 py-2 text-sm leading-tight border ${item.active ? 'hover:bg-primary-100 hover:text-primary-700 border-gray-700 bg-gray-700 text-white' : 'bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white'}`}
-                >{item.label}</a
+                >{item.label}</PaginationLink
               >
-            </li>
+            </PaginationItem>
           {/if}
         {/each}
-        <li>
-          <a
-            href={products.next_page_url}
+        <PaginationItem>
+          <PaginationNext
             class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span class="sr-only">Next</span>
-            <svg
-              class="w-5 h-5"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-        </li>
-      </ul>
-    </nav>
+            href={products.next_page_url}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
   </div>
 </div>
---
+++ resources/js/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  unidad_medida: string;
+  stock: number;
+  activo: boolean;
+  created_at: string;
+  updated_at: string;
+}
